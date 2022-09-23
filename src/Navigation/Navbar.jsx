06b8dadfd39a9ed656efa862cbb91849794b06ea
ana crandall <107437409+annacrandall@users.noreply.github.com>
2022-09-23 14:51:52 -0500

    Created popover for nav bar.

--- src/Navigation/Navbar.jsx
+++ src/Navigation/Navbar.jsx
@@ -1,31 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Popover, Transition } from '@headlessui/react'
 
 
 const Navbar = () => {
     return (
-        <nav className="m-5 ">
-            <ul className="flex flex-row justify-center gap-3">
+        <nav className="m-3">
+            <ul className="flex flex-row justify-center gap-20">
                 <li>
                     <Link to="/">Home</Link>
                 </li>
-                <li>
-                    <Link to="/drumkit">Drumkit</Link>
-                </li>
-                <li>
-                    <Link to="/quotes">Quote Generator</Link>
-                </li>
-                <li>
-                    <Link to="/calculator">Calculator</Link>
-                </li>
-                <li>
-                    <Link to="/pomodoro">Pomodoro Clock</Link>
-                </li>
-                <li>
-                    <Link to="/markdown">Markdown Previewer</Link>
-                </li>
+                <Popover className="relative">
+                    <Popover.Button>Projects</Popover.Button>
+                    <Transition
+                        enter="transition duration-100 ease-out"
+                        enterFrom="transform scale-95 opacity-0"
+                        enterTo="transform scale-100 opacity-100"
+                        leave="transition duration-75 ease-out"
+                        leaveFrom="transform scale-100 opacity-100"
+                        leaveTo="transform scale-95 opacity-0" >
+
+                        <Popover.Panel className="absolute text-center">
+                        <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                            <div className="relative grid gap-8 bg-white p-7 lg:grid-cols-1">
+                                <Link to="/drumkit" className="flex h-10 w-10 shrink-0 items-center justify-center text-black sm:h-12 sm:w-12">Drumkit</Link>
+                                <Link to="/quotes" className="flex h-10 w-10 shrink-0 items-center justify-center text-black sm:h-12 sm:w-12">Quote Generator</Link>
+                                <Link to="/calculator" className="flex h-10 w-10 shrink-0 items-center justify-center text-black sm:h-12 sm:w-12">Calculator</Link>
+                                <Link to="/pomodoro" className="flex h-10 w-10 shrink-0 items-center justify-center text-black sm:h-12 sm:w-12">Pomodoro Clock</Link>
+                                <Link to="/markdown" className="flex h-10 w-10 shrink-0 items-center justify-center text-black sm:h-12 sm:w-12">Markdown Previewer</Link>
+                                </div>
+                                </div>
+                        </Popover.Panel>
+                    </Transition>
+                </Popover>
             </ul>
         </nav>
-    ); 
+    );
 }
 export default Navbar; 
