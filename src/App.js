63ecded15c9727494562e4920b0e9dc01cfc8273
ana crandall <107437409+annacrandall@users.noreply.github.com>
2022-09-28 16:44:04 -0500

    Render pages using React router.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Navigation/Navbar"; 
-//import Drumkit from "./pages/Drumkit";
-import Calculator from "./pages/Calculator"; 
-import Markdown from "./pages/Markdown"; 
-import Quotes from "./pages/Quotes"; 
-import Pomodoro from "./pages/Pomodoro";
 import NoPage from "./pages/NoPage"; 
 import Home from "./pages/Home"
 
+const RedirectUrl = ({ url }) => {
+  useEffect(() => {
+    window.location.href = url;
+  }, [url]);
+
+  return <h1 className="text-center">Redirecting...</h1>;
+};
 
 const App = () => {
-  const Drumkit = () => {
-    window.location.href('https://google.com')
-  }
  
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route exact path="/"  element={<Home />} />
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/drumkit" element={<RedirectUrl url="https://crandall-drumkit.netlify.app" />} />
+        <Route exact path="/calculator" element={<RedirectUrl url="https://crandall-calculator.netlify.app" />} />
+        <Route exact path="/pomodoro" element={<RedirectUrl url="https://crandall-pomodoro-timer.netlify.app" />} />
+        <Route exact path="/quotes" element={<RedirectUrl url="https://breaking-bad-quote-gen.netlify.app" />} />
+        <Route exact path="/markdown" element={<RedirectUrl url="https://crandall-markdown-previewer.netlify.app" />}/>
         <Route path="/*" element={<NoPage />} /> 
       </Routes>
     </Router>
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+  
+const Home = () => {
+  const background={
+    backgroundImage:`url(${process.env.PUBLIC_URL+ "/background.jpg"})`,
+            height:'100vh',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            };
+  return (
+    <div style={background}>
+      <div className="flex justify-center" >
+      <div className="m-5 p-6 bg-amber-50 max-w-lg rounded-lg border border-gray-200 shadow-lg ">
+          <h1 className="">Welcome </h1>
+          <p className="leading-loose leading-9"></p>
+        </div>
+        </div>
+    </div>
+  );
+};
+  
+export default Home;
