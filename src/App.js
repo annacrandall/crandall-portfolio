a683586e836b51ce6da6f5991be112106e9e8450
ana crandall <107437409+annacrandall@users.noreply.github.com>
2022-09-29 14:07:14 -0500

    Removed unneccesary routes from App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import React, {useEffect} from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./Navigation/Navbar"; 
+import Navbar from "./components/Navigation/Navbar"; 
 import NoPage from "./pages/NoPage"; 
-import Home from "./pages/Home"
+import Home from "./pages/Home"; 
+import Contact from "./pages/Contact"; 
 
-const RedirectUrl = ({ url }) => {
-  useEffect(() => {
-    window.location.href = url;
-  }, [url]);
-
-  return <h1 className="text-center">Redirecting...</h1>;
-};
 
 const App = () => {
  
@@ -19,12 +13,8 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route exact path="/drumkit" element={<RedirectUrl url="https://crandall-drumkit.netlify.app" />} />
-        <Route exact path="/calculator" element={<RedirectUrl url="https://crandall-calculator.netlify.app" />} />
-        <Route exact path="/pomodoro" element={<RedirectUrl url="https://crandall-pomodoro-timer.netlify.app" />} />
-        <Route exact path="/quotes" element={<RedirectUrl url="https://breaking-bad-quote-gen.netlify.app" />} />
-        <Route exact path="/markdown" element={<RedirectUrl url="https://crandall-markdown-previewer.netlify.app" />}/>
         <Route path="/*" element={<NoPage />} /> 
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
