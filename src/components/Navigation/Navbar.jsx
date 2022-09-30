d06f7f219fe23d2c6ab6a960dfed884105e25b4c
ana crandall <107437409+annacrandall@users.noreply.github.com>
2022-09-30 14:58:58 -0500

    Added styling to 404 page.

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -1,25 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Popover, Transition } from '@headlessui/react'
+import { Popover} from '@headlessui/react'
 
 
 const Navbar = () => {
     return (
-        <nav className="m-3">
+        <nav className="m-3 border-2 border-black p-2">
             <ul className="flex flex-row justify-center gap-20">
                 <li>
                     <Link to="/" className="font-medium">Home</Link>
                 </li>
                 <Popover className="relative z-50">
                     <Popover.Button className="font-medium">Projects</Popover.Button>
-                    <Transition
-                        enter="transition duration-100 ease-out"
-                        enterFrom="transform scale-95 opacity-0"
-                        enterTo="transform scale-100 opacity-100"
-                        leave="transition duration-75 ease-out"
-                        leaveFrom="transform scale-100 opacity-100"
-                        leaveTo="transform scale-95 opacity-0" >
-
                         <Popover.Panel className="absolute left-1/2 z-50 mt-3 w-screen max-w-sm -translate-x-1/2 transform px-4 sm:px-0 lg:max-w-3xl">
                         <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
                             <div className="relative grid gap-8 bg-white p-7 lg:grid-cols-2">
@@ -31,8 +23,8 @@ const Navbar = () => {
                                 </div>
                                 </div>
                         </Popover.Panel>
-                    </Transition>
                 </Popover>
+                <Link to="/contac" className="font-medium">Contact</Link>
             </ul>
         </nav>
     );
--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NoPage = () => {
     return (
-        <div className='text-center'>
-            <h1>Error 404</h1>
-            <p>This page does not exist or cannot be found.</p>
+        <div className='tracking-wide text-lg'>
+            <div className='text-center bg-red-700 ml-20 mr-20 mt-5 mb-5 p-3 rounded'>
+                <h1 className='text-3xl text-white font-error'>Error 404</h1>
+            </div>
+
+            <div id='intro' className='text-center font-body'>
+            <p>Uh oh, I'm sorry... I made a mistake. </p>
+             <p>The page you're looking for could have moved, been deleted, or maybe it never existed!! </p>
+            <h2 className='mt-10'>This is your last chance, after this there is no going back:</h2>
+            </div>
+
+
+            <div className='mt-5 font-body grid grid-cols-2 text-center' id='choices'>
+                <div id='home-container'>
+                    <p>You can go back to the home page...</p>
+                    <Link to='/'><button className='border-2 rounded-full p-3 bg-blue-500/75 mt-5'>Go home.</button></Link>
+                </div>
+                <div id='contact-container'>
+                    <p>or you can contact me and tell me what happened!</p>
+                    <Link to='/contact'><button className='border-2 rounded-full p-3 bg-red-500/75 mt-5'>Deliver the bad news.</button></Link>
+                 </div>
+             </div>
         </div>
     );
 }
